12328613005104a0d3537ca5bb788ccdd7fa049b
creator <leha_sing@mail.ru>
2019-06-21 13:44:33 +0300

    add 2 annotations in CityRepo

--- src/main/java/com/example/TravelManager/repository/CityRepo.java
+++ src/main/java/com/example/TravelManager/repository/CityRepo.java
@@ -2,7 +2,12 @@ package com.example.TravelManager.repository;
 
 import com.example.TravelManager.model.City;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
+@Repository
+@Transactional
 public interface CityRepo extends JpaRepository<City, Integer> {
     City findByCityName(String cityName);
 }
